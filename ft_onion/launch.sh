2e2ce742b17f382042be454748dc3b55c0c07736
Massilia <massblri@gmail.com>
2024-01-16 18:09:08 +0100

    not finish

--- ft_onion/launch.sh
+++ ft_onion/launch.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-echo "PrivateNetwork=yes" >> /etc/init.d/nginx.service
-tor -f /etc/tor/torrc
-/bin/bash
+sudo nft -f /etc/nftables.conf
+service tor start
+onion_hostname=$(cat /var/lib/tor/onion/hostname)
+echo "server_name $onion_hostname;" > serverName
+sed -i 's/server_name _;/'"$(cat serverName)"'/' /etc/nginx/nginx.conf
+
+service nginx reload
+
+#nginx -g 'daemon off;'
+/bin/sh
