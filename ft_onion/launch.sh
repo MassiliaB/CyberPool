1424c436ba7830a66d2c4912939bdd659b413718
Massilia <massblri@gmail.com>
2024-01-17 15:29:56 +0100

    tor circuit ok

--- ft_onion/launch.sh
+++ ft_onion/launch.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
-sudo nft -f /etc/nftables.conf
+#sudo nft -f /etc/nftables.conf
+echo "hello"
 service tor start
 onion_hostname=$(cat /var/lib/tor/onion/hostname)
 echo "server_name $onion_hostname;" > serverName
 sed -i 's/server_name _;/'"$(cat serverName)"'/' /etc/nginx/nginx.conf
+echo "PrivateNetwork=yes" > /lib/systemd/system/nginx.service
 
 service nginx reload
-
+#tor -f /etc/tor/torrc
 #nginx -g 'daemon off;'
 /bin/sh
