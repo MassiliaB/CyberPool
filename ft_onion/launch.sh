3f282f409ab926b9ff99e32f1ce0dc367fd6f654
Massilia <massblri@gmail.com>
2024-01-18 10:50:08 +0100

    fixing still

--- ft_onion/launch.sh
+++ ft_onion/launch.sh
@@ -6,8 +6,12 @@ service tor start
 onion_hostname=$(cat /var/lib/tor/onion/hostname)
 echo "server_name $onion_hostname;" > serverName
 sed -i 's/server_name _;/'"$(cat serverName)"'/' /etc/nginx/nginx.conf
+
 echo "PrivateNetwork=yes" > /lib/systemd/system/nginx.service
-#ln -sf /etc/nginx/sites-available/
+rm /etc/nginx/sites-available/defaut
+rm /etc/nginx/sites-enabled/defaut
+
 nginx -t
+echo "****Hostname: $onion_hostname*****\n"
 #nginx -g 'daemon off;'
 /bin/bash
