04d1f30e7d9480b24682638987b4c1479b35ed1c
Massilia <massblri@gmail.com>
2024-01-18 12:57:06 +0100

    fixing nginx config

--- ft_onion/launch.sh
+++ ft_onion/launch.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-#sudo nft -f /etc/nftables.conf
-echo "hello"
 service tor start
 onion_hostname=$(cat /var/lib/tor/onion/hostname)
 echo "server_name $onion_hostname;" > serverName
-sed -i 's/server_name _;/'"$(cat serverName)"'/' /etc/nginx/nginx.conf
+sed -i 's/server_name _;/'"$(cat serverName)"'/' /etc/nginx/sites-enabled/onion.conf
+
+rm /etc/nginx/sites-available/default
+rm /etc/nginx/sites-enabled/default
+ln -sf /etc/nginx/sites-enabled/onion.conf /etc/nginx/sites-available/
 
 nginx -t
-echo "****Hostname: $onion_hostname*****\n"
+echo "Hostname: $onion_hostname\n"
 #nginx -g 'daemon off;'
 /bin/bash
