fa33b0954eb81ce988fb771577b56d7c9e092fe8
Massilia <massblri@gmail.com>
2024-01-18 11:43:04 +0100

    deleting some config

--- ft_onion/launch.sh
+++ ft_onion/launch.sh
@@ -7,10 +7,6 @@ onion_hostname=$(cat /var/lib/tor/onion/hostname)
 echo "server_name $onion_hostname;" > serverName
 sed -i 's/server_name _;/'"$(cat serverName)"'/' /etc/nginx/nginx.conf
 
-echo "PrivateNetwork=yes" > /lib/systemd/system/nginx.service
-rm /etc/nginx/sites-available/defaut
-rm /etc/nginx/sites-enabled/defaut
-
 nginx -t
 echo "****Hostname: $onion_hostname*****\n"
 #nginx -g 'daemon off;'
