101340fea64c5735bb716dc28bc8760b058d0a28
Massilia <massblri@gmail.com>
2024-01-18 15:17:34 +0100

    all clear

--- ft_onion/launch.sh
+++ ft_onion/launch.sh
@@ -4,10 +4,12 @@ service tor start
 onion_hostname=$(cat /var/lib/tor/onion/hostname)
 echo "server_name $onion_hostname;" > serverName
 sed -i 's/server_name _;/'"$(cat serverName)"'/' /etc/nginx/sites-available/onion.conf
-
 ln -sf /etc/nginx/sites-available/onion.conf /etc/nginx/sites-enabled/onion.conf
+mkdir /var/run/sshd
+
+sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
 
 nginx -t
 echo "Hostname: $onion_hostname\n"
+/usr/sbin/sshd -D
 nginx -g 'daemon off;'
-#/bin/bash
