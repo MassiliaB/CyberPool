3bacd105f7bb933ca49a7fc784472dcc3ef99abc
Massilia <massblri@gmail.com>
2024-01-24 20:33:26 +0100

    entropie and disk read ok

--- irondome/irondome.py
+++ irondome/irondome.py
@@ -14,7 +14,9 @@ import math
 from threading import Thread, Event
 
 log_file_path = "log/irondome.log"
-logging.basicConfig(filename=log_file_path, level=logging.INFO)
+initial_disk_state = None
+initial_entropy_state = None
+logging.basicConfig(filename=log_file_path, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def log_alert(message): # enregistre les erreurs dans les logs
     logging.info(message)
@@ -23,46 +25,36 @@ def memory_limit():
     memory_limit_bytes = 100 * 1024 * 1024
     resource.setrlimit(resource.RLIMIT_AS, (memory_limit_bytes, memory_limit_bytes))
 
-def monitor_disk_read_abuse(): # check le disk usage
+def get_disk_state(): # Etat actuel du disque
     stats = os.popen("cat /sys/block/sda/stat").read()
     stats_split = stats.split(' ')
-    stats_array = list(filter(None, stats_split))
+    return list(filter(None, stats_split))    
 
-    stats_2 = os.popen("cat /sys/block/sda/stat").read()
-    stats_split_2 = stats_2.split(' ')
-    stats_array_2 = list(filter(None, stats_split_2))
-    iototal = [
-    int(stats_array_2[0]) - int(stats_array[0]),
-    int(stats_array_2[1]) - int(stats_array[1]),
-    int(stats_array_2[2]) - int(stats_array[2]),
-    int(stats_array_2[3]) - int(stats_array[3]),
-    int(stats_array_2[4]) - int(stats_array[4]),
-    int(stats_array_2[5]) - int(stats_array[5]),
-    int(stats_array_2[6]) - int(stats_array[6]),
-    int(stats_array_2[7]) - int(stats_array[7]),
-    int(stats_array_2[8]) - int(stats_array[8]),
-    int(stats_array_2[9]) - int(stats_array[9]),
-    int(stats_array_2[10]) - int(stats_array[10])
-    ]
+def monitor_disk_read_abuse(): # check le disk usage
+    global initial_disk_state
+    if initial_disk_state is None:
+        initial_disk_state = get_disk_state()
 
-    description = [
-    "\n-----------Disk read monitoring----------\n",
-    "IO stats over last " + str(timer) + " seconds \n\n",
-    "read I/Os : " + str(iototal[0]),
-    "read merges : " + str(iototal[1]),
-    "read sectors : " + str(iototal[2]),
-    "read ticks (ms) : " + str(iototal[3]),
-    "write I/Os : " + str(iototal[4]),
-    "write merges : " + str(iototal[5]),
-    "write sectors : " + str(iototal[6]),
-    "write ticks (ms) : " + str(iototal[7]),
-    "in_flight : " + str(iototal[8]),
-    "io_ticks (ms) : " + str(iototal[9]),
-    "time_in_queue (ms) : " + str(iototal[10]),
-    "\n---------------------\n"
-    ]
-    for d in description:
-        log_alert(d)
+    current_disk_state = get_disk_state()
+    if current_disk_state != initial_disk_state:
+        description = [
+        "\n-----------Disk read monitoring----------\n",
+        "IO stats over last 60 seconds \n\n",
+        "read I/Os : " + str(current_disk_state[0]),
+        "read merges : " + str(current_disk_state[1]),
+        "read sectors : " + str(current_disk_state[2]),
+        "read ticks (ms) : " + str(current_disk_state[3]),
+        "write I/Os : " + str(current_disk_state[4]),
+        "write merges : " + str(current_disk_state[5]),
+        "write sectors : " + str(current_disk_state[6]),
+        "write ticks (ms) : " + str(current_disk_state[7]),
+        "in_flight : " + str(current_disk_state[8]),
+        "io_ticks (ms) : " + str(current_disk_state[9]),
+        "time_in_queue (ms) : " + str(current_disk_state[10]),
+        "\n---------------------\n"
+        ]
+        for d in description:
+            log_alert(d)
 
 # def monitor_crypto_activity(): # check l'activité intensive crypto
 #     try:
@@ -96,41 +88,37 @@ def monitor_disk_read_abuse(): # check le disk usage
 #             Logger.write_log_files(log_output_directory)
 #         FileLister.cleanup_all_tmp_files()
 
-def process_file(file_path):
-    try:
-        with open(file_path, "rb") as file:
-            counters = {byte: 0 for byte in range(2 ** 8)} 
-            for byte in file.read():
-                counters[byte] += 1
-            filesize = file.tell()
-            probabilities = [counter / filesize for counter in counters.values()]
-            entropy = -sum(probability * math.log2(probability) for probability in probabilities if probability > 0)
-            log_alert("\n-----------Entropy monitoring----------\n")
-            log_alert(f"Entropy of {os.path.basename(file_path)}: {entropy}")
-    except Exception as expn:
-        error_message = "\n-----------File entropy monitoring----------\n"
-        error_message = error_message + f"Error processing file {file_path}: {str(expn)}\n\n{traceback.format_exc()}"
-        print(error_message)
-        log_alert(error_message)
+def get_file_entropy(file_path): # l'entropie d'un fichier
+    with open(file_path, "rb") as file:
+        counters = {byte: 0 for byte in range(2 ** 8)} 
+        for byte in file.read():
+            counters[byte] += 1
+        filesize = file.tell()
+        probabilities = [counter / filesize for counter in counters.values()]
+        entropy = -sum(probability * math.log2(probability) for probability in probabilities if probability > 0)
+        return entropy
 
-def entropy_change(paths):
-    try:
-        while True:
-            for path in paths:
-                if os.path.isfile(path):
-                    process_file(path)
-                elif os.path.isdir(path):
-                    files_in_directory = [os.path.join(path, file) for file in os.listdir(path) if os.path.isfile(os.path.join(path, file))]
-                    for file_path in files_in_directory:
-                        process_file(file_path)
-                else:
-                    log_alert("\n-----------File entropy monitoring----------\n")
-                    log_alert(f"Invalid path: {path}")
-    except Exception as expn:
-        error_message = "\n-----------File entropy monitoring----------\n"
-        error_message = error_message + f"Error in entropy_change: {str(expn)}\n\n{traceback.format_exc()}"
-        print(error_message)
-        log_alert(error_message)
+def get_entropy_state(paths): # l'etat actuel de l'entropie
+    entropy_values = []
+    for path in paths:
+        if os.path.isfile(path):
+            entropy_values.append(get_file_entropy(path))
+        elif os.path.isdir(path):
+            files_in_directory = [os.path.join(path, file) for file in os.listdir(path) if os.path.isfile(os.path.join(path, file))]
+            for file_path in files_in_directory:
+                entropy_values.append(get_file_entropy(file_path))
+    return entropy_values
+
+def monitor_entropy(paths):
+    global initial_entropy_state
+    if initial_entropy_state is None:
+        initial_entropy_state = get_entropy_state(paths)
+
+    current_entropy_state = get_entropy_state(paths)
+    if current_entropy_state != initial_entropy_state:
+        log_alert("\n-----------Entropy monitoring----------\n")
+        for idx, entropy_value in enumerate(current_entropy_state, start=1):
+            log_alert(f"Entropy of file {idx}: {entropy_value}")
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
@@ -147,7 +135,7 @@ def main():
     paths = parse_arguments()
     while True:
         monitor_disk_read_abuse()
-        entropy_change(paths)
+        monitor_entropy(paths)
         time.sleep(60)
     # monitor_crypto_activity()
 
